e1fcca04e0a44b6d6d5364537123e525bb907261
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-01-04 12:17:45 -0800

    Class Completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,8 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+// const { expect } = require("@jest/globals");
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -14,30 +17,69 @@
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
-
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
 // Example input: 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+    describe("fibonacci", ()=>{
+        it(
+          "takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.",()=>{
+            expect(fibonacci(6)).toEqual([1, 1, 2, 3, 5, 8]);
+            expect(fibonacci(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+          });
+    });
 
 // b) Create the function that makes the test pass.
-
+    // Declare a function fib that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+    const fibonacci = (num) => {
+      if (num < 2) {
+        return "try a  bigger number";
+      }
+      // declare a local variable to add to the end of the array that starts at one and gets added to itself as it goes up in a for loop
+      let currentNumber = 1;
+      // declare an array to store the values
+      let fibNums = [1];
+      // create a variable to store the value of the previous number in the sequence
+      let lastNum = 1;
+      // for loop will take in the parameter an return an array that pushes the currentNumber on to the array and then adds to currentNumber
+      for (let index = 1; index < num; index++) {
+          fibNums.push(currentNumber)
+          //add current number to previous number to get the new current number
+          currentNumber = currentNumber + lastNum
+          lastNum = currentNumber - lastNum
+      }
+      return fibNums
+    }
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
+    describe("oddNumbers", () => {
+      it("takes an input array of any type and outputs an array that shows odd numbers from least to greatest", () => {
+          let fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+          let fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+        expect(oddNumbers(fullArr1)).toEqual([-9, 7, 9, 199]);
+        expect(oddNumbers(fullArr2)).toEqual([-823, 7, 23]);
+      });
+    });
 
-var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+// Expected output: [-9, 7, 9, 199]
 // Expected output: [-823, 7, 23]
 
-
 // b) Create the function that makes the test pass.
-
+// This function will take an input array of any type and outputs an array that shows odd numbers from least to greatest
+    const oddNumbers = (array) => {
+        // will require a filter and the condiotn is value %2 !== 0  and  data type must equal "number"
+        // a sort fucntion will be implemeted to sort numbers from least to greatest 
+        // sorted array will be returned
+        // let filteredArr = array.filter(value => value % 2 !== 0 && typeof value === 'number')
+        // let sortedFilter = filteredArr.sort((a, b) => a - b);
+        // return sortedFilter;
+         return array.filter((value) => value % 2 !== 0 && typeof value === "number").sort((a, b) => a - b);
+    }
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
@@ -52,5 +94,30 @@ var numbersToAdd2 = [0, 7, -8, 12]
 var numbersToAdd3 = []
 // Expected output: []
 
+describe("someFunction", () => {
+  it("takes an array of numbers and returns an arry with each number having the value of the previous value addedd to it", () => {
+    let numbersToAdd1 = [2, 4, 45, 9]
+    let numbersToAdd2 = [0, 7, -8, 12]
+    let numbersToAdd3 = []
+    expect(someFunction(numbersToAdd1)).toEqual([2, 6, 51, 60]);
+    expect(someFunction(numbersToAdd2)).toEqual([0, 7, -1, 11]);
+    expect(someFunction(numbersToAdd3)).toEqual([]);
+  });
+});
 
 // b) Create the function that makes the test pass.
+// create someFunction 
+    const someFunction = (array) => {
+        // Create an if statement for the edge case
+        if (array.length < 1) {
+            return []
+        } 
+        // create a new array variable and set index 0 to what it already is 
+        let summed = [array[0]]
+        // itterate over the array starting with Index 1 
+        for (let index = 1; index < array.length; index++) {
+            // push the value of array at i with the pervious value into the new array
+            summed.push(array[index] + summed[index - 1])
+        }
+        return summed
+    }
--- interview-questions.md
+++ interview-questions.md
@@ -11,7 +11,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: "This" keyword refers to the object it belongs too. It has different value depending on where it's used. This refers tyo an object which is an instant of class itself. This is used in react constantly, it is ued in constantly to the object state or props live inside of. 
 
 
 
@@ -19,49 +19,49 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
   Your answer:
 
-  Researched answer:
+  Researched answer: React is a front end JS framework created by developers at facebook to change the way user interact with websites. React alters the webpage through the virtual dom. Thus eliminating the need for websites to reload every time there is a change-- such as clicking a like button. React facilitates creating fast interactive applications and is heavily focused on the application interactive UI. 
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: Render
 
-  Researched answer:
+  Researched answer: Render method is the only required method in a react class component. The render method returns JSX to the user so that the React component can display something. Render does not modify the component state itself. It is only a return of the current state of the class object. 
 
 
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: JSX gives the ability to write and add HTML in react via JS. Converting HTML tags into react elements. JSX is an extension to the JS language. JSX stands for JavaScript XML. XML stands for XHTML, which is an HTML extension. 
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: Yarn is 'Yet another Resource Negotiator'. Yarn is a package manager that helps to manage project dependency. Yarn is a fast reliable and secure dependency management tool that was developed by Facebook and is open source. A benefit of yarn is it stores dependency locally, so that you don't download packages multiple times. When required users can easily reinstall because it fetches from the disk when executing the yarn add command. The files and folders modified are:   package.json- Which keeps track of our dependencies, yarn.lock - which tracks which dependencies have already been installed, and node_modules which are the dependencies themselves.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
+  Researched answer: A function without a name. A use case is preventing a immediate invocation.  The syntax for an anonymous function is the ES6 Fat arrow syntax.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: A component that will only run and render to the screen when predetermined conditions are met. 
 
-2. Props:
+2. Props: Arguments passed into react components. They are passed using HTML attributes. 
 
-3. JavaScript Events:
+3. JavaScript Events: What happens when a user interacts with a page. Events are when JS is activated within HTML. 
 
-4. Object-oriented programming:
+4. Object-oriented programming: Is a model that revolves around designing your code around your data. Instead of using logical equation the code is built around the expected data. 
 
-5. Ruby:
+5. Ruby: an OOP language which is also 'Open Sourced'. It has a reputation for having easily readability. Ruby will allow you to update your local version of source the code as you desire. 
